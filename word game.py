8149542df274fe261ac5f31c28061bfaaf2040df
victorwahome <133209942+victorwahome@users.noreply.github.com>
2024-05-17 16:31:20 +0300

    Add files via upload

---
+++ word game.py
@@ -0,0 +1,6 @@
+verb1=input("write a verb\n ")
+verb2=input("write another verb\n ")
+verb3=input("write the final verb\n ")
+game=f"in the morning i woke up and {verb1} down the stairs. i {verb2} my breakfast then proceded to school. " \
+     f"on the way a random man {verb3} into a river which surprised me very much"
+print(game)
